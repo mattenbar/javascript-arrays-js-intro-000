a3ff12919eabd48dd67dcf465957c5f5e4563ff1
Matthew Enbar <matthew.enbar@gmail.com>
2020-03-18 20:24:58 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -34,7 +34,7 @@ function destructivelyRemoveElementFromBeginningOfArray (array) {
   return array
 }
 
-function removeElementFromBeginningOfArray (array) (array) {
+function removeElementFromBeginningOfArray (array){
   //non destructive
   array.slice()
   return array
