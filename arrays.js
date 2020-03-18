63d306d159b3b9e5a1aadef46bbe425c54d3724f
Matthew Enbar <matthew.enbar@gmail.com>
2020-03-18 17:51:59 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -11,5 +11,5 @@ function addElementToBeginningOfArray (array, element){
 }
 
 function destructivelyAddElementToBeginningOfArray (array, element){
-  array = [element, ...array]
+  array = array.unshift(element)
 }
