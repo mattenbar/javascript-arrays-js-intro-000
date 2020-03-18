ae0fd704b8525b418a41a217cc7b5c58f6cee58c
Matthew Enbar <matthew.enbar@gmail.com>
2020-03-18 19:17:16 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,46 +1,7 @@
-var chocolateBars = [
-  'snickers',
-  'hundred grand',
-  'kitkat',
-  'skittles'
-]
-
-function addElementToBeginningOfArray (array, element){
-  [element,...array]
-  return array
+function addElementToBeginningOfArray (){
 
 }
 
-function destructivelyAddElementToBeginningOfArray (array, element){
-  [element,...array]
-  return array
-
-}
-
-function addElementToEndOfArray(array, element){
-
-}
-
-function destructivelyAddElementToEndOfArray(array, element){
-
-}
-
-function accessElementInArray(array, index){
-
-}
-
-function destructivelyRemoveElementFromBeginningOfArray(array){
-
-}
-
-function removeElementFromBeginningOfArray(array){
-
-}
-
-function destructivelyRemoveElementFromEndOfArray(array){
-
-}
-
-function removeElementFromEndOfArray(array){
-
+function destructivelyAddElementToBeginningOfArray (){
+  
 }
